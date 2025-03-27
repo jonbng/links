648572c81cb15c6d2e6bf5801a48d8bf1dc8fa74
Jonathan Bangert <jonathan@bangert.dk>
2025-03-27 10:02:48 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,21 +10,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Alfa Beta",
   description:
-    "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @arctixdev.",
+    "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @jonba_.",
   applicationName: "Alfa Beta",
   themeColor: "#FFFFFF",
   colorScheme: "light",
   creator: "Jonathan Bangert",
   publisher: "Jonathan Bangert",
-  keywords: ["toolbox", "alfa", "beta", "arctixdev"],
+  keywords: ["toolbox", "alfa", "beta", "jonba_"],
   viewport: "width=device-width, initial-scale=1.0",
   twitter: {
-    site: "@arctixdev",
-    creator: "@arctixdev",
+    site: "@jonba_",
+    creator: "@jonba_",
     card: "summary_large_image",
-    creatorId: "arctixdev",
-    description: "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @arctixdev.",
-    siteId: "arctixdev",
+    creatorId: "jonba_",
+    description: "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @jonba_.",
+    siteId: "jonba_",
     title: "Alfa Beta",
     images: [
       {
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     locale: "en_US",
     url: "https://alfabeta.dk",
     title: "Alfa Beta",
-    description: "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @arctixdev.",
+    description: "The only link shortener you'll ever need. Feature rich and minimalistic. - Made with ❤️ by @jonba_.",
     siteName: "Alfa Beta",
     emails: "contact@alfabeta.dk",
     images: [
@@ -72,8 +72,8 @@ export default function RootLayout({
         <footer className="text-center text-sm text-muted-foreground py-4 fixed bottom-0 left-0 right-0">
           <p>
             Made with ❤️ by{" "}
-            <a href="https://x.com/arctixdev" target="_blank" className="hover:underline">
-              @arctixdev
+            <a href="https://x.com/jonba_" target="_blank" className="hover:underline">
+              @jonba_
             </a>
           </p>
         </footer>
